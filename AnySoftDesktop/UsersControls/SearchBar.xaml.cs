836b894e9e9af96e7e9086724991a4a8d6798fb8
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-02 16:13:14 +0300

    Added UserControl custom SearchBar

---
+++ AnySoftDesktop/UsersControls/SearchBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace AnySoftDesktop.UsersControls;
+
+public partial class SearchBar : UserControl
+{
+    public SearchBar()
+    {
+        InitializeComponent();
+    }
+}
