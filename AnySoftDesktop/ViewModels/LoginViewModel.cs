0a657568c1a96229b97c65f63648585ae618452f
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-02 20:32:49 +0300

    Modified login page

--- AnySoftDesktop/ViewModels/LoginViewModel.cs
+++ AnySoftDesktop/ViewModels/LoginViewModel.cs
@@ -1,24 +1,57 @@
-﻿using AnySoftDesktop.ViewModels.Framework;
+﻿using System;
+using System.IO;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading;
+using AnySoftDesktop.Services;
+using AnySoftDesktop.ViewModels.Framework;
+using RPM_Project_Backend.Domain;
 
 namespace AnySoftDesktop.ViewModels;
 
 public class LoginViewModel : DialogScreen
 {
-    public string Login
+    private readonly DialogManager _dialogManager;
+    private readonly IViewModelFactory _viewModelFactory;
+
+    public LoginViewModel(DialogManager dialogManager, IViewModelFactory viewModelFactory)
     {
-        get;
-        set;
+        _dialogManager = dialogManager;
+        _viewModelFactory = viewModelFactory;
     }
-    
-    public string Email
+
+    public UserDto UserCredentials { get; set; } = new UserDto();
+
+    public void CloseView()
     {
-        get;
-        set;
+        Close(false);
     }
-    
-    public string Password
+
+    public async void AccountLogin()
     {
-        get;
-        set;
-    }
+        var result = await WebApiService.PostCall("/auth/login", UserCredentials);
+        if (result.IsSuccessStatusCode)
+        {
+            var timeoutAfter = TimeSpan.FromMilliseconds(100);
+            using var cancellationTokenSource = new CancellationTokenSource(timeoutAfter);
+            
+            var responseStream = await result.Content.ReadAsStreamAsync(cancellationTokenSource.Token);
+            var responseResult = await JsonSerializer.DeserializeAsync<JwtResponseModel>(responseStream, cancellationToken: cancellationTokenSource.Token);
+            Close(true);
+        }
+        else
+        {
+            var messageBoxDialog = _viewModelFactory.CreateMessageBoxViewModel(
+                title: "Some error has occurred",
+                message: $@"An error occurred while making a request to the server".Trim(),
+                okButtonText: "OK",
+                cancelButtonText: null
+            );
+            await _dialogManager.ShowDialogAsync(messageBoxDialog);
+        }
+    } 
+    public async void AccountRegister()
+    {
+
+    } 
 }
