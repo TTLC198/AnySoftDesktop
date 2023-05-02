3f5bf584c67533627754325f1fe0127bca24d51e
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-02 16:13:22 +0300

    Added UserControl custom TextBox

---
+++ AnySoftDesktop/UsersControls/TextBoxWithPlaceholder.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace AnySoftDesktop.UsersControls;
+
+public partial class TextBoxWithPlaceholder : UserControl
+{
+    [Description("Textbox corner radius"), Category("Appearance")]
+    public int CornerRadius
+    {
+        get; set;
+    }
+    
+    [Description("Textbox placeholder"), Category("Appearance")]
+    public string? Placeholder
+    {
+        get; set;
+    }
+
+    public TextBoxWithPlaceholder()
+    {
+        InitializeComponent();
+        this.DataContext = this;
+    }
+}
