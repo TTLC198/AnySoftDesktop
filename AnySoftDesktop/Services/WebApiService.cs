6e33ad74d99aa6b8fe654923630b946dbc258077
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-04 12:09:25 +0300

    Added try catch

--- AnySoftDesktop/Services/WebApiService.cs
+++ AnySoftDesktop/Services/WebApiService.cs
@@ -9,55 +9,85 @@ namespace AnySoftDesktop.Services;
 
 public class WebApiService
 {
-    public static async Task<HttpResponseMessage> GetCall(string url)  
+    public static async Task<HttpResponseMessage> GetCall(string url, string authorizatonToken = "")  
     {  
         ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;  
         var apiUrl = App.ApiUrl + url;
-        using var client = new HttpClient();
-        client.BaseAddress = new Uri(apiUrl);  
-        client.Timeout = TimeSpan.FromSeconds(900);  
-        client.DefaultRequestHeaders.Accept.Clear();  
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));  
-        var response = await client.GetAsync(apiUrl);   
-        return response;
+        try
+        {
+            using var client = new HttpClient();
+            if (authorizatonToken != string.Empty)
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {authorizatonToken}");
+            client.BaseAddress = new Uri(apiUrl);  
+            client.Timeout = TimeSpan.FromSeconds(900);  
+            client.DefaultRequestHeaders.Accept.Clear();  
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));  
+            var response = await client.GetAsync(apiUrl);   
+            return response;
+        }
+        catch
+        {
+            throw;
+        }
     } 
     
     public static async Task<HttpResponseMessage> PostCall<T>(string url, T model) where T : class
     {
         var apiUrl = App.ApiUrl + url;
         ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-        using var client = new HttpClient();
-        client.BaseAddress = new Uri(apiUrl);  
-        client.Timeout = TimeSpan.FromSeconds(900);  
-        client.DefaultRequestHeaders.Accept.Clear();  
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));  
-        var response = await client.PostAsJsonAsync(apiUrl, model);  
-        return response;
+        try
+        {
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri(apiUrl);  
+            client.Timeout = TimeSpan.FromSeconds(900);  
+            client.DefaultRequestHeaders.Accept.Clear();  
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));  
+            var response = await client.PostAsJsonAsync(apiUrl, model);  
+            return response;
+        }
+        catch
+        {
+            throw;
+        }
     }  
     
     public static async Task<HttpResponseMessage> PutCall<T>(string url, T model) where T : class  
     {  
         ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;  
         var apiUrl = App.ApiUrl + url;
-        using var client = new HttpClient();
-        client.BaseAddress = new Uri(apiUrl);  
-        client.Timeout = TimeSpan.FromSeconds(900);  
-        client.DefaultRequestHeaders.Accept.Clear();  
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));  
-        var response = await client.PutAsJsonAsync(apiUrl, model); 
-        return response;
+        try
+        {
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri(apiUrl);  
+            client.Timeout = TimeSpan.FromSeconds(900);  
+            client.DefaultRequestHeaders.Accept.Clear();  
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));  
+            var response = await client.PutAsJsonAsync(apiUrl, model); 
+            return response;
+        }
+        catch
+        {
+            throw;
+        }
     }  
     
     public static async Task<HttpResponseMessage> DeleteCall(string url)   
     {  
         ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;  
         var apiUrl = App.ApiUrl + url;
-        using var client = new HttpClient();
-        client.BaseAddress = new Uri(apiUrl);  
-        client.Timeout = TimeSpan.FromSeconds(900);  
-        client.DefaultRequestHeaders.Accept.Clear();  
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));  
-        var response = await client.DeleteAsync(apiUrl);
-        return response;
+        try
+        {
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri(apiUrl);  
+            client.Timeout = TimeSpan.FromSeconds(900);  
+            client.DefaultRequestHeaders.Accept.Clear();  
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));  
+            var response = await client.DeleteAsync(apiUrl);
+            return response;
+        }
+        catch
+        {
+            throw;
+        }
     } 
 }
