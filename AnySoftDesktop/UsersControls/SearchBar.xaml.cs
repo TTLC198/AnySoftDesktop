37150da51bfd383263f63d9f7f2253feaa96c3dd
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-04 12:07:52 +0300

    Fixed Searchbar style, added SearchBar text property

--- AnySoftDesktop/UsersControls/SearchBar.xaml.cs
+++ AnySoftDesktop/UsersControls/SearchBar.xaml.cs
@@ -1,9 +1,25 @@
-﻿using System.Windows.Controls;
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace AnySoftDesktop.UsersControls;
 
 public partial class SearchBar : UserControl
 {
+    [Description("Searchbar Text"), Category("Text")]
+    public string SearchText
+    {
+        get => (string)GetValue(SearchTextProperty);
+        set => SetValue(SearchTextProperty, value);
+    }
+
+    public static readonly DependencyProperty SearchTextProperty =
+        DependencyProperty.Register(
+            "SearchText",
+            typeof(string), 
+            typeof(TextBoxWithPlaceholder),
+            new UIPropertyMetadata(""));
+    
     public SearchBar()
     {
         InitializeComponent();
