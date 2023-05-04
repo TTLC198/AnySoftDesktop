d35d68e2966b14368b8cfd1cd529dec2a8071e9e
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-04 12:09:46 +0300

    Fixed web api link

--- AnySoftDesktop/App.xaml.cs
+++ AnySoftDesktop/App.xaml.cs
@@ -21,7 +21,7 @@ namespace AnySoftDesktop
 
         public static Version Version { get; } = Assembly.GetName().Version!;
         
-        public static string ApiUrl { get; } = "https://localhost:5001/api";
+        public static string ApiUrl { get; } = "https://localhost:5001/";
 
         public static string VersionString { get; } = "v" + Version.ToString(3).Trim();
 
