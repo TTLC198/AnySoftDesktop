bff571ebf4fa2501e600028f655f46f6f2acb3d7
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-04 21:31:49 +0300

    Added ItemCarousel.cs model

---
+++ AnySoftDesktop/ViewModels/Framework/ItemCarousel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace AnySoftDesktop.ViewModels.Framework;
+
+public class ItemCarousel: INotifyPropertyChanged
+{
+    private bool _isSelected;
+    public bool IsSelected
+    {
+        get => _isSelected;
+        set
+        {
+            _isSelected = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private string _image;
+    public string Image
+    {
+        get => _image;
+        set
+        {
+            _image = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
