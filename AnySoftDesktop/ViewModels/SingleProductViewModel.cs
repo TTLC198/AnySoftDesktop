b3a6071f2b3bab49fa82f25862109e481713df65
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-07 19:26:06 +0300

    Added SingleProductView

---
+++ AnySoftDesktop/ViewModels/SingleProductViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using AnySoftDesktop.Services;
+using AnySoftDesktop.Utils;
+using AnySoftDesktop.ViewModels.Framework;
+using AnySoftDesktop.ViewModels.Tabs;
+using RPM_Project_Backend.Domain;
+
+namespace AnySoftDesktop.ViewModels;
+
+public class SingleProductViewModel : INotifyPropertyChanged
+{
+    private readonly int Id;
+    private readonly IViewModelFactory _viewModelFactory;
+    private readonly DialogManager _dialogManager;
+
+    private ProductResponseDto _product = new();
+
+    public ProductResponseDto Product
+    {
+        get => _product;
+        set
+        {
+            _product = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public SingleProductViewModel(int id, IViewModelFactory viewModelFactory, DialogManager dialogManager)
+    {
+        Id = id;
+        _viewModelFactory = viewModelFactory;
+        _dialogManager = dialogManager;
+    }
+
+    public async void OnViewFullyLoaded()
+    {
+        await UpdateProduct();
+    }
+
+    private async Task UpdateProduct()
+    {
+        var getProductsRequest = await WebApiService.GetCall($"api/products/{Id}");
+
+        try
+        {
+            if (getProductsRequest.IsSuccessStatusCode)
+            {
+                var timeoutAfter = TimeSpan.FromMilliseconds(300);
+                using var cancellationTokenSource = new CancellationTokenSource(timeoutAfter);
+                var responseStream = await getProductsRequest.Content.ReadAsStreamAsync(cancellationTokenSource.Token);
+                var product = await JsonSerializer.DeserializeAsync<ProductResponseDto>(responseStream,
+                    CustomJsonSerializerOptions.Options, cancellationToken: cancellationTokenSource.Token);
+                Product = product ?? throw new InvalidOperationException("Product is null");
+            }
+            else
+            {
+                throw new InvalidOperationException(await getProductsRequest.Content.ReadAsStringAsync());
+            }
+        }
+        catch (Exception exception)
+        {
+            var messageBoxDialog = _viewModelFactory.CreateMessageBoxViewModel(
+                title: "Some error has occurred",
+                message: $@"{exception.Message}".Trim(),
+                okButtonText: "OK",
+                cancelButtonText: null
+            );
+            await _dialogManager.ShowDialogAsync(messageBoxDialog);
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
---
+++ AnySoftDesktop/Views/SingleProductView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace AnySoftDesktop.Views;
+
+public partial class SingleProductView : UserControl
+{
+    public SingleProductView()
+    {
+        InitializeComponent();
+    }
+}
