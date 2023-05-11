aedce99bbd8841caf90994d2786326d4622fd43a
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-11 23:24:51 +0300

    Fixed hide button and added Tray menu

--- AnySoftDesktop/Views/MainWindowView.xaml.cs
+++ AnySoftDesktop/Views/MainWindowView.xaml.cs
@@ -11,12 +11,28 @@ public partial class MainWindowView : Window
         InitializeComponent();
     }
     
+    protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+    {
+        //clean up notifyicon (would otherwise stay open until application finishes)
+        MainTaskbarIcon.Dispose();
+        base.OnClosing(e);
+    }
+    
     private void HeaderBorder_OnMouseDown(object sender, MouseButtonEventArgs e)
     {
         if (e.ChangedButton == MouseButton.Left)
             DragMove();
     }
+    
+    private void MainTaskbarIcon_OnTrayBalloonTipClicked(object sender, RoutedEventArgs e)
+    {
+        Visibility = 
+            Visibility == Visibility.Visible
+                ? Visibility.Hidden 
+                : Visibility.Visible;
+    }
 
-    private void HideButton_OnClick(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
-    private void CloseButton_OnClick(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+    private void Show_OnClick(object sender, RoutedEventArgs e) => Show();
+    private void Hide_OnClick(object sender, RoutedEventArgs e) => Hide();
+    private void Close_OnClick(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
 }
