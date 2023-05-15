8797921dbd9781f3202c66a576996a7deb61e9da
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-15 10:58:30 +0300

    Added new dialog for purchase

---
+++ AnySoftDesktop/ViewModels/ProductPurchaseDialogViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text.Json;
+using System.Threading;
+using AnySoftDesktop.Models;
+using AnySoftDesktop.Services;
+using AnySoftDesktop.Utils;
+using AnySoftDesktop.ViewModels.Framework;
+using AnySoftDesktop.ViewModels.Tabs;
+using RPM_Project_Backend.Domain;
+using Stylet;
+
+namespace AnySoftDesktop.ViewModels;
+
+public class ProductPurchaseDialogViewModel : DialogScreen<Payment>, INotifyPropertyChanged
+{
+    private readonly DialogManager _dialogManager;
+    private readonly IViewModelFactory _viewModelFactory;
+
+    public IReadOnlyList<Payment> PaymentMethods { get; }
+
+    public ProductPurchaseDialogViewModel(DialogManager dialogManager, IViewModelFactory viewModelFactory, IReadOnlyList<Payment> paymentMethods)
+    {
+        _dialogManager = dialogManager;
+        _viewModelFactory = viewModelFactory;
+        PaymentMethods = paymentMethods;
+    }
+    
+    public async void OnViewFullyLoaded()
+    {
+        
+    }
+
+    public void CloseView()
+    {
+        Close(null!);
+    }
+}
---
+++ AnySoftDesktop/Views/ProductPurchaseDialogView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace AnySoftDesktop.Views;
+
+public partial class ProductPurchaseDialogView : UserControl
+{
+    public ProductPurchaseDialogView()
+    {
+        InitializeComponent();
+    }
+}
