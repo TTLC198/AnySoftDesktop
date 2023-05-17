f8f2f58bdb8dde44de47caec676c4800c705599e
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-17 22:33:07 +0300

    Added/Fixed converters

---
+++ AnySoftDesktop/Utils/Converters/InverseBoolToBoolConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AnySoftDesktop.Utils.Converters;
+
+[ValueConversion(typeof(bool), typeof(bool))]
+public class InverseBoolToBoolConverter : IValueConverter
+{
+    public static InverseBoolToBoolConverter Instance { get; } = new();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is not true;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is not true;
+}
--- AnySoftDesktop/Utils/Converters/NullToVisibilityConverter.cs
+++ AnySoftDesktop/Utils/Converters/NullToVisibilityConverter.cs
@@ -12,7 +12,7 @@ public class NullToVisibilityConverter : IValueConverter
     
     public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
     {
-        return value is null  ? Visibility.Collapsed : Visibility.Visible;
+        return value is null ? Visibility.Collapsed : Visibility.Visible;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
