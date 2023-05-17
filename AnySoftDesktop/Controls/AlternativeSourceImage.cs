1f173331b17606dcc19995fb46a976e2dc3758ca
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-18 00:05:33 +0300

    Added support for an alternate image if the image cannot be loaded from the server

---
+++ AnySoftDesktop/Controls/AlternativeSourceImage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace AnySoftDesktop.Controls;
+
+public class AlternativeSourceImage : Image
+{
+    private bool _tryAlternativeSource = true;
+
+    public ImageSource AlternativeSource
+    {
+        get => (ImageSource)GetValue(AlternativeSourceProperty);
+        set => SetValue(AlternativeSourceProperty, value);
+    }
+
+    // Using a DependencyProperty as the backing store for ItemTemplate.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty AlternativeSourceProperty =
+        DependencyProperty.Register(nameof(AlternativeSource), typeof(ImageSource), typeof(AlternativeSourceImage), new PropertyMetadata(null));
+
+    public AlternativeSourceImage()
+    {
+        Initialized += OnInitialized;
+    }
+
+    private void OnInitialized(object sender, EventArgs eventArgs)
+    {
+        //Note , ths need to be unregistered 
+        ImageFailed += OnImageFailed;
+    }
+
+    private void OnImageFailed(object sender, ExceptionRoutedEventArgs exceptionRoutedEventArgs)
+    {
+        if (!_tryAlternativeSource)
+            return;
+
+        _tryAlternativeSource = false;
+        
+        Source = AlternativeSource;
+    }
+}
