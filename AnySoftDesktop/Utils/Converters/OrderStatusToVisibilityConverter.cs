26c7451100eecdcc44896d98e667c13b171a5e92
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-19 22:11:11 +0300

    Added OrderStatusToVisibilityConverter

---
+++ AnySoftDesktop/Utils/Converters/OrderStatusToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AnySoftDesktop.Utils.Converters;
+
+[ValueConversion(typeof(string), typeof(Visibility))]
+public class OrderStatusToVisibilityConverter : IValueConverter
+{
+    public static OrderStatusToVisibilityConverter Instance { get; } = new();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return 
+            value is string and "Paid" 
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}
