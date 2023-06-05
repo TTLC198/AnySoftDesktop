35aa9f9c8574934bafe81b5e687ce3149386567d
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-06-05 17:46:45 +0300

    Added ability to change Api & Cdn Urls

--- AnySoftDesktop/ViewModels/Tabs/SettingsTabViewModel.cs
+++ AnySoftDesktop/ViewModels/Tabs/SettingsTabViewModel.cs
@@ -1,4 +1,5 @@
-﻿using AnySoftDesktop.ViewModels.Framework;
+﻿using AnySoftDesktop.Utils;
+using AnySoftDesktop.ViewModels.Framework;
 
 namespace AnySoftDesktop.ViewModels.Tabs;
 
@@ -6,6 +7,18 @@ public class SettingsTabViewModel : TabBaseViewModel
 {
     private readonly IViewModelFactory _viewModelFactory;
     private readonly DialogManager _dialogManager;
+
+    public string? ApiUrl 
+    {
+        get => VersionManager.Instance.ApiUrl;
+        set => VersionManager.Instance.ApiUrl = value;
+    }
+    
+    public string? CdnUrl
+    {
+        get => VersionManager.Instance.CdnUrl;
+        set => VersionManager.Instance.CdnUrl = value;
+    }
     
     public async void OnViewFullyLoaded()
     {
