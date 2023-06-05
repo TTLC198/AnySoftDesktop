84005e219cdcf3ef32afa6330f28a5eff457de2c
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-06-05 17:46:12 +0300

    Added static version manager to handle urls updates

---
+++ AnySoftDesktop/Utils/VersionManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using AnySoftDesktop.Models;
+
+namespace AnySoftDesktop.Utils;
+
+public class VersionManager : DependencyObject
+{
+    private static readonly DependencyProperty ApiUrlProperty =
+        DependencyProperty.Register( 
+            nameof(ApiUrl), 
+            typeof(string),
+            typeof(VersionManager));
+
+    public string? ApiUrl
+    {
+        get => (string)GetValue(ApiUrlProperty);
+        set => SetValue(ApiUrlProperty, value );
+    }
+    
+    private static readonly DependencyProperty CdnUrlProperty =
+        DependencyProperty.Register( 
+            nameof(CdnUrl), 
+            typeof(string),
+            typeof(VersionManager));
+    
+    public string? CdnUrl
+    {
+        get => (string)GetValue(CdnUrlProperty);
+        set => SetValue(CdnUrlProperty, value );
+    }
+
+    public static VersionManager Instance { get; private set; }
+
+    static VersionManager() {
+        Instance = new VersionManager();
+    }
+}
